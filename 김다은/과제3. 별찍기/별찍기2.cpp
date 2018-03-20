b349500f626915fc959927d8253c4ea1d41adcc5
DAEUN18 <aco1128@naver.com>
2018-03-20 22:40:18 +0900

    별찍기

---
+++ 김다은/과제3. 별찍기/별찍기2.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	
+	int i = 0;
+	int j = 0;
+		
+	for(i = 0; i < 5; i++){
+		
+		for(j = 0; j < 5 - i; j++){
+			
+			printf("*");
+		}
+		
+		printf("\n");
+	}
+}
