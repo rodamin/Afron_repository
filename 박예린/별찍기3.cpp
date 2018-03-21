f028dddcbf3ffda56f8a61b3889988bb2402de99
Yerin-park <yerin4847@naver.com>
2018-03-21 17:23:10 +0900

    별찍기2

--- 박예린/별찍기3.cpp
+++ 박예린/별찍기3.cpp
@@ -4,14 +4,16 @@ main()
 {
 	int i = 0;
 	int o = 0;
+
 	
 	for(i = 0; i < 5; i++)
 	{
-		for(o = 0; o < 5 - i; o++)
-		{
-		printf("*");
-		}
+		for(o = 0; o < 4 - i; o++)
+		printf(" ");
 		
+		for(o = 0; o <= i; o++)
+		printf("*");
+				
 		printf("\n");
 	}
 }
