dea0956054e4d964535a82063e0268aa56865277
DAEUN18 <aco1128@naver.com>
2018-03-21 21:51:05 +0900

    스택1

---
+++ 김다은/과제4. 스택만들기/스택1.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#define MAX_SIZE 80
+int stack[MAX_SIZE];
+
+int top = -1;
+
+void push(int p){
+
+	if(top >= MAX_SIZE){
+		return;
+	}
+		
+	stack[++top] = p; 
+
+}
+
+void pop(){
+	
+	if(top < 0){
+		return;
+	}
+	
+	printf("%d", stack[top]);
+	--top;
+}
+
+int main(){
+	
+	int a;
+	scanf("%d", &a);
+	
+	push(a);
+	pop();
+	
+
+}
