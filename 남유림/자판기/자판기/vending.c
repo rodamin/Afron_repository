ae0b0943f828e3b19e91e818df6538cc16ebcf3a
milkyway002 <sea_bom_12@naver.com>
2018-03-27 21:51:18 +0900

    자판기 최종

---
+++ 남유림/자판기/자판기/vending.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+struct _item {
+	char name[32];
+	int cost;
+	int stub;
+}items[32] = { 0 };
+
+//void addItem(char, int, int);
+
+int main(void) {
+	puts("1. 관리자\t2. 사용자");
+
+}
